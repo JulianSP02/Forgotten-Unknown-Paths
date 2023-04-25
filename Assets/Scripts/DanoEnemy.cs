aaa905752647bedf4ca5753c1ce0eed92ddfd85c
DaniFelipe1510 <danielballesterocor@gmail.com>
2023-04-24 21:47:03 -0600

    Mecánica Policia
    Falta animación

---
+++ Assets/Scripts/DanoEnemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DanoEnemy : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+
+            SceneManager.LoadScene
+               (SceneManager.GetActiveScene().buildIndex);
+
+        }
+
+    }
+}
--- Assets/Scripts/PatrolController.cs
+++ Assets/Scripts/PatrolController.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class PatrolController : MonoBehaviour
 {
+    public float rangoDeAlerta;
+    public LayerMask capaDelJugador;
+    bool estarAlerta;
+    public Transform jugador;
+    public float velocidad;
+
+    void Update()
+    {
+        estarAlerta = Physics.CheckSphere(transform.position, rangoDeAlerta, capaDelJugador);
+
+        if (estarAlerta == true)
+        {
+            //transform.LookAt(jugador);
+            Vector3 posJugador = new Vector3(jugador.position.x, transform.position.y, jugador.position.z);
+            transform.LookAt(posJugador);
+            transform.position = Vector3.MoveTowards
+                (transform.position, posJugador
+                , velocidad * Time.deltaTime);
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, rangoDeAlerta);
+    }
+
     [Header("Animation")]
     [SerializeField]
     Animation animator;
